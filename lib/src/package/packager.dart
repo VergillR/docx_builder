fbb0234e8f41e5827dd1572a71dcbb36f9ca8cc5
vergillr <vergillrl@gmail.com>
2020-07-22 16:08:38 +0200

    Updated packager

--- lib/src/package/packager.dart
+++ lib/src/package/packager.dart
@@ -50,26 +50,31 @@ class Packager {
       final Directory sourceDirectory =
           Directory('${cacheDirectory.path}/$cacheDocXBuilder/$src');
 
-      File('${cacheDirectory.path}/$cacheDocXBuilder/$src/[Content_Types].xml')
-          .writeAsStringSync(ContentTypes().getContentsTypesXml());
-      File('$_dirPathToDocProps/app.xml').writeAsStringSync(AppXml(
+      final File a = File(
+          '${cacheDirectory.path}/$cacheDocXBuilder/$src/[Content_Types].xml')
+        ..createSync();
+      a.writeAsStringSync(ContentTypes().getContentsTypesXml());
+      final File b = File('$_dirPathToDocProps/app.xml')..createSync();
+      b.writeAsStringSync(AppXml(
         chars: chars,
         charsWithSpaces: charsWithSpaces,
         paragraphs: paragraphs,
       ).getAppXml());
-      File('$_dirPathToDocProps/core.xml')
-          .writeAsStringSync(CoreXml().getCoreXml());
-      File('$_dirPathToWordRels/document.xml.rels')
-          .writeAsStringSync(DocumentXmlRels().getDocumentXmlRels());
-      File('$_dirPathToWord/document.xml').writeAsStringSync(documentXml);
-      File('$_dirPathToWord/fontTable.xml')
-          .writeAsStringSync(FontTable().getFontTableXml());
-      File('$_dirPathToWord/settings.xml')
-          .writeAsStringSync(SettingsXml().getSettingsXml());
-      File('$_dirPathToWord/styles.xml')
-          .writeAsStringSync(WordStylesXml().getWordStylesXml());
+      final File c = File('$_dirPathToDocProps/core.xml')..createSync();
+      c.writeAsStringSync(CoreXml().getCoreXml());
+      final File d = File('$_dirPathToWordRels/document.xml.rels')
+        ..createSync();
+      d.writeAsStringSync(DocumentXmlRels().getDocumentXmlRels());
+      final File e = File('$_dirPathToWord/document.xml')..createSync();
+      e.writeAsStringSync(documentXml);
+      final File f = File('$_dirPathToWord/fontTable.xml')..createSync();
+      f.writeAsStringSync(FontTable().getFontTableXml());
+      final File g = File('$_dirPathToWord/settings.xml')..createSync();
+      g.writeAsStringSync(SettingsXml().getSettingsXml());
+      final File h = File('$_dirPathToWord/styles.xml')..createSync();
+      h.writeAsStringSync(WordStylesXml().getWordStylesXml());
 
-      final File docxFile = File(destination);
+      final File docxFile = File(destination)..createSync();
       await ZipFile.createFromDirectory(
           sourceDir: sourceDirectory, zipFile: docxFile, recurseSubDirs: true);
       return docxFile;
