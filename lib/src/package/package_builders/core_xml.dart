39bd5dfaa84aa3c50342193502490a0739dc2ec3
vergillr <vergillrl@gmail.com>
2020-07-28 00:30:35 +0200

    Updated core.xml

--- lib/src/package/package_builders/core_xml.dart
+++ lib/src/package/package_builders/core_xml.dart
@@ -3,26 +3,11 @@ import 'package:docx_builder/src/utils/constants/constants.dart';
 class CoreXml {
   CoreXml();
 
-  String getCoreXml(
-      {String documentTitle,
-      String documentSubject,
-      String documentDescription,
-      String documentCreator}) {
-    // ignore: prefer_final_locals
-    String a = docPropsCoreXml;
-    if (documentTitle.isNotEmpty) {
-      a.replaceFirst('<dc:title>', '<dc:title>$documentTitle');
-    }
-    if (documentSubject.isNotEmpty) {
-      a.replaceFirst('<dc:subject>', '<dc:subject>$documentSubject');
-    }
-    if (documentDescription.isNotEmpty) {
-      a.replaceFirst(
-          '<dc:description>', '<dc:description>$documentDescription');
-    }
-    if (documentCreator.isNotEmpty) {
-      a.replaceFirst('<dc:creator>', '<dc:creator>$documentCreator');
-    }
-    return a;
-  }
+  String getCoreXml({
+    String documentTitle = '',
+    String documentSubject = '',
+    String documentDescription = '',
+    String documentCreator = '',
+  }) =>
+      '$docPropsCoreXml<dc:title>$documentTitle</dc:title><dc:subject>$documentSubject</dc:subject><dc:description>$documentDescription</dc:description><dc:creator>$documentCreator</dc:creator></cp:coreProperties>';
 }
--- lib/src/package/package_builders/document_xml_rels.dart
+++ lib/src/package/package_builders/document_xml_rels.dart
@@ -3,17 +3,6 @@ import 'package:docx_builder/src/utils/constants/constants.dart';
 class DocumentXmlRels {
   DocumentXmlRels();
 
-  // TODO: Add more references when needed, for example: media and hyperlinks.
-  /* For example:
-  <Relationship Id="rId4" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/image" Target="media/image3.png"/><Relationship Id="rId5" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/image" Target="media/image4.png"/><Relationship Id="rId6" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/image" Target="media/image5.png"/><Relationship Id="rId7" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/image" Target="media/image6.png"/><Relationship Id="rId8" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/fontTable" Target="fontTable.xml"/><Relationship Id="rId9" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/settings" Target="settings.xml"/>
-  */
-  /* Hyperlink example: 
-    <Relationship Id="rId2" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/hyperlink" Target="http://www.ns.nl/" TargetMode="External"/>
-  */
-  /* Header and footer example (files header.xml and footer.xml go in the same folder as document.xml):
-    <Relationship Id="rId4" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/header" Target="header3.xml"/>
-    <Relationship Id="rId5" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/footer" Target="footer1.xml"/>
-  */
   String getDocumentXmlRels(Map<String, String> references) {
     const String closeTag = '</Relationships>';
     if (references.isNotEmpty) {
